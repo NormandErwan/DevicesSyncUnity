0934f2621426c39c69e8a336ca9d4d26d4dba900
Erwan Normand <normand.erwan@protonmail.com>
2017-07-18 12:04:22 -0400

    Initialized the project.

---
+++ Middleware/Middleware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Middleware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
