20a1ad226e12f52d8bcad714f0cc40669fd7439c
Erwan Normand <normand.erwan@protonmail.com>
2017-07-18 15:55:59 -0400

    Added a minimal example of middleware SignalR server.

---
+++ Middleware/Middleware/Hub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DeviceSyncUnity.Middleware
+{
+    public class Hub : Microsoft.AspNet.SignalR.Hub
+    {
+        public void Send(string name, string message)
+        {
+            Program.Log("send");
+            Clients.All.AddMessage(name, message);
+        }
+
+        public dynamic RequestReplyDynamic()
+        {
+            return new { time = DateTime.Now.ToLongTimeString() };
+        }
+
+        public int RequestReplyValueType()
+        {
+            return DateTime.Now.Millisecond;
+        }
+    }
+}
--- Middleware/Middleware/Program.cs
+++ Middleware/Middleware/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 
-namespace Middleware
+namespace DeviceSyncUnity.Middleware
 {
     class Program
     {
+        private const string url = "http://*:8080";
+        private const int mainLoopEndDelay = 10;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (WebApp.Start<Startup>(url))
+            {
+                Log("Device synchronization for Unity - Middleware");
+                Log("Running on " + url);
+                Log("Press Esc to exit");
+
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape) { }
+            }
+        }
+
+        internal static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
         }
     }
 }
