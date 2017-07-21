b855266caa2425ece595bd65a85bc7b4fc47a5ae
Erwan Normand <normand.erwan@protonmail.com>
2017-07-20 23:10:38 -0400

    [Client] Added a ClientBase script to connect async to the middleware and an example script.

---
+++ DeviceSyncUnity/Assets/DeviceSyncUnity/Scripts/ClientBase.cs
@@ -0,0 +1,91 @@
+﻿using SignalR.Client._20.Hubs;
+using System;
+using System.Threading;
+using UnityEngine;
+
+namespace DeviceSyncUnity
+{
+    public abstract class ClientBase : MonoBehaviour
+    {
+        // Editor fields
+
+        [SerializeField]
+        protected string hubConnectionUrl = "http://localhost:8080";
+
+        [SerializeField]
+        protected string hubName = "Hub";
+
+        [SerializeField]
+        protected bool autoStart = true;
+
+        // Event
+
+        public EventHandler Connected = delegate { };
+
+        // Properties
+
+        public HubConnection Connection { get; protected set; }
+
+        public IHubProxy Proxy { get; protected set; }
+
+        public bool IsConnected { get; protected set; }
+
+        // Variables
+
+        protected Thread connectionThread;
+
+        protected object connectionLock = new object();
+
+        protected bool threadConnecting;
+
+        // Methods
+
+        public void ConnectionStartAsync()
+        {
+            connectionThread.Start();
+        }
+
+        protected virtual void Awake()
+        {
+            Connection = new HubConnection(hubConnectionUrl);
+            Proxy = Connection.CreateProxy(hubName);
+
+            connectionThread = new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                lock (connectionLock)
+                {
+                    threadConnecting = true;
+                }
+
+                Connection.Start();
+
+                lock (connectionLock)
+                {
+                    IsConnected = true;
+                }
+            });
+        }
+
+        protected virtual void Start()
+        {
+            if (autoStart)
+            {
+                ConnectionStartAsync();
+            }
+        }
+
+        protected virtual void Update()
+        {
+            lock (connectionLock)
+            {
+                if (IsConnected && threadConnecting)
+                {
+                    threadConnecting = false;
+                    Connected.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
