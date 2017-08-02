4798e4a4ea48e5314d124d4f374a195d518619b0
Erwan Normand <normand.erwan@protonmail.com>
2017-08-02 15:10:42 -0400

    Added a logger to log events from another thread in Debug.Log and in a UI console like logger.

---
+++ DeviceSyncUnity/Assets/DeviceSyncUnity/Scripts/Utilities/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DeviceSyncUnity
+{
+    public class Logger : Singleton<Logger>
+    {
+        public Text text;
+
+        private List<string> lines = new List<string>();
+
+        public static void Log(string message)
+        {
+            Debug.Log(message);
+
+            lock (Instance.lines)
+            {
+                Instance.lines.Add(message + "\n");
+            }
+        }
+
+        private void Start()
+        {
+            text.text = "";
+        }
+
+        private void Update()
+        {
+            lock (Instance.lines)
+            {
+                foreach (var line in lines)
+                {
+                    Instance.text.text += line;
+                }
+                lines.Clear();
+            }
+        }
+    }
+}
