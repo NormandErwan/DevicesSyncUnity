f66f35c700d57bfd8f82c140efea62f7a4842a17
Erwan Normand <normand.erwan@protonmail.com>
2017-08-17 23:58:09 -0400

    Completed the TouchMessage struct.

--- Assets/DeviceSyncUnity/Scripts/TouchMessage.cs
+++ Assets/DeviceSyncUnity/Scripts/TouchMessage.cs
@@ -8,19 +8,58 @@ namespace DeviceSyncUnity
 
         public TouchMessage(Touch touch)
         {
+            fingerId = touch.fingerId;
+            position = touch.position;
+            rawPosition = touch.rawPosition;
+            deltaPosition = touch.deltaPosition;
+            deltaTime = touch.deltaTime;
+            tapCount = touch.tapCount;
             phase = touch.phase;
+            pressure = touch.pressure;
+            maximumPossiblePressure = touch.maximumPossiblePressure;
+            type = touch.type;
+            altitudeAngle = touch.altitudeAngle;
+            azimuthAngle = touch.azimuthAngle;
+            radius = touch.radius;
+            radiusVariance = touch.radiusVariance;
         }
 
         // Properties
 
+        public int fingerId { get; set; }
+        public Vector2 position { get; set; }
+        public Vector2 rawPosition { get; set; }
+        public Vector2 deltaPosition { get; set; }
+        public float deltaTime { get; set; }
+        public int tapCount { get; set; }
         public TouchPhase phase { get; set; }
+        public float pressure { get; set; }
+        public float maximumPossiblePressure { get; set; }
+        public TouchType type { get; set; }
+        public float altitudeAngle { get; set; }
+        public float azimuthAngle { get; set; }
+        public float radius { get; set; }
+        public float radiusVariance { get; set; }
 
         // Methods
 
         public Touch GetTouch()
         {
             Touch touch = new Touch();
+            touch.fingerId = fingerId;
+            touch.position = position;
+            touch.rawPosition = rawPosition;
+            touch.deltaPosition = deltaPosition;
+            touch.deltaTime = deltaTime;
+            touch.tapCount = tapCount;
             touch.phase = phase;
+            touch.pressure = pressure;
+            touch.maximumPossiblePressure = maximumPossiblePressure;
+            touch.type = type;
+            touch.altitudeAngle = altitudeAngle;
+            touch.azimuthAngle = azimuthAngle;
+            touch.radius = radius;
+            touch.radiusVariance = radiusVariance;
             return touch;
         }
     }
