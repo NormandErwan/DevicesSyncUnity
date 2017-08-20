99e2beac90e888d3e1e8854f4bea9ee22f6e6397
Erwan Normand <normand.erwan@protonmail.com>
2017-08-20 10:41:13 -0400

    Implemented frame interval for sending touches in SmartphonePCSync.

--- Assets/DeviceSyncUnity/Scripts/SmartphonePCSync.cs
+++ Assets/DeviceSyncUnity/Scripts/SmartphonePCSync.cs
@@ -7,6 +7,13 @@ using UnityEngine.Networking.NetworkSystem;
 
 namespace DeviceSyncUnity
 {
+    public enum SendTouchesMode
+    {
+        NoSending,
+        TimeInterval,
+        FramesInterval
+    }
+
     [RequireComponent(typeof(NetworkIdentity))]
     public class SmartphonePCSync : NetworkBehaviour
     {
@@ -15,16 +22,22 @@ namespace DeviceSyncUnity
         [SerializeField]
         private NetworkManager networkManager;
 
-        // TODO: choose between an time interval or a frame interval
+        [SerializeField]
+        private SendTouchesMode sendTouchesMode = SendTouchesMode.TimeInterval;
 
         [SerializeField]
-        private float sendTouchesInterval = 0.1f;
+        private float sendTouchesTimeInterval = 0.1f;
+
+        [SerializeField]
+        private uint sendTouchesFramesInterval = 1;
 
         // Properties
 
         public NetworkManager NetworkManager { get { return networkManager; } set { TryStartSync(value); } }
 
-        public float SendTouchesInterval { get { return sendTouchesInterval; } set { sendTouchesInterval = value; } }
+        public SendTouchesMode SendTouchesMode { get { return sendTouchesMode; } set { sendTouchesMode = value; } }
+        public float SendTouchesTimeInterval { get { return sendTouchesTimeInterval; } set { sendTouchesTimeInterval = value; } }
+        public uint SendTouchesFramesInterval { get { return sendTouchesFramesInterval; } set { sendTouchesFramesInterval = value; } }
 
         public Dictionary<int, TouchesMessage> Touches { get; protected set; }
 
@@ -36,10 +49,72 @@ namespace DeviceSyncUnity
 
         // Variables
 
+        protected uint sendTouchesFramesCount = 0;
+        protected float sendTouchesTimer = 0;
         protected bool touchesLastFrames = false;
         protected bool syncStarted = false;
 
         // Methods
+
+        public virtual void SendTouches(TouchesMessage touchesMessage = null)
+        {
+            if (NetworkManager == null || NetworkManager.client == null || !isClient)
+            {
+                Debug.LogError("Can't send touches : no NetworkManager, or not connected as client.");
+                return;
+            }
+
+            if (touchesMessage == null)
+            {
+                touchesMessage = GetTouchesMessage();
+            }
+            Debug.Log("Send touches (count: " + touchesMessage.touches.Length + ")", LogFilter.Debug);
+            NetworkManager.client.Send(MessageType.Touches, touchesMessage);
+        }
+
+        public virtual TouchesMessage GetTouchesMessage()
+        {
+            var message = new TouchesMessage();
+            message.connectionId = NetworkManager.client.connection.connectionId;
+            message.PopulateFromInput();
+            message.PopulateFromCamera(Camera.main);
+            return message;
+        }
+
+        public virtual IEnumerator SendTouchesWithInterval()
+        {
+            while (Input.touchSupported && SendTouchesMode != SendTouchesMode.NoSending)
+            {
+                if (Input.touchCount == 0 && !touchesLastFrames)
+                {
+                    yield return null;
+                }
+                touchesLastFrames = Input.touchCount != 0;
+
+                var message = GetTouchesMessage();
+                if (SendTouchesMode == SendTouchesMode.FramesInterval)
+                {
+                    sendTouchesFramesCount++;
+                    if (sendTouchesFramesCount >= SendTouchesFramesInterval)
+                    {
+                        sendTouchesFramesCount = 0;
+                        SendTouches(message);
+                    }
+                }
+                else if (SendTouchesMode == SendTouchesMode.TimeInterval)
+                {
+                    if (Time.unscaledTime - sendTouchesTimer >= SendTouchesTimeInterval)
+                    {
+                        sendTouchesTimer = Time.unscaledTime;
+                        SendTouches(message);
+                    }
+                }
+
+                // TODO: stack values
+                // TODO: send also an average of the stack values
+                yield return null;
+            }
+        }
 
         // TODO: handle client reconnection
         protected virtual void Start()
@@ -53,6 +128,8 @@ namespace DeviceSyncUnity
             // Unsubscribe from the previous NetworkManager
             if (syncStarted)
             {
+                StopCoroutine("SendTouchesWithInterval");
+
                 NetworkManager.client.UnregisterHandler(MessageType.Touches);
                 NetworkManager.client.UnregisterHandler(MsgType.Error);
                 networkManager = newNetworkManager;
@@ -73,42 +150,9 @@ namespace DeviceSyncUnity
                 {
                     NetworkManager.client.RegisterHandler(MessageType.Touches, ReceiveTouches);
                     Debug.Execute(() => NetworkManager.client.RegisterHandler(MsgType.Error, OnError), LogFilter.Error);
-
-                    if (Input.touchSupported)
-                    {
-                        StartCoroutine("SendTouchesCoroutine");
-                    }
+                    StartCoroutine("SendTouchesWithInterval");
                 }
             }
-        }
-
-        protected virtual IEnumerator SendTouchesCoroutine()
-        {
-            while (NetworkManager != null && NetworkManager.client != null)
-            {
-                SendTouches();
-                // TODO: stack values every frame and send the stack
-                // TODO: send also an average of the stack values
-                yield return new WaitForSeconds(SendTouchesInterval);
-            }
-        }
-
-        protected void SendTouches()
-        {
-            if (Input.touchCount == 0 && !touchesLastFrames)
-            {
-                return;
-            }
-
-            var message = new TouchesMessage();
-            message.connectionId = NetworkManager.client.connection.connectionId;
-            message.PopulateFromInput();
-            message.PopulateFromCamera(Camera.main);
-
-            Debug.Log("Send touches (count: " + message.touches.Length + ")", LogFilter.Debug);
-
-            touchesLastFrames = message.touches.Length != 0;
-            NetworkManager.client.Send(MessageType.Touches, message);
         }
 
         protected virtual void SendToAllClientsTouches(NetworkMessage netMessage)
