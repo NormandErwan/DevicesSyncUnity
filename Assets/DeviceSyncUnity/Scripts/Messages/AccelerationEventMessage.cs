a125858aeaaefdd1af2faf026071b41d8ccf5ae5
Erwan Normand <normand.erwan@protonmail.com>
2017-08-25 11:22:05 -0400

    Added forgotten file AccelerationEventMessage.cs

---
+++ Assets/DeviceSyncUnity/Scripts/Messages/AccelerationEventMessage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DeviceSyncUnity.Messages
+{
+    public class AccelerationEventMessage
+    {
+        // Constructor
+
+        public AccelerationEventMessage()
+        {
+        }
+
+        public AccelerationEventMessage(AccelerationEvent accelerationEvent)
+        {
+            acceleration = accelerationEvent.acceleration;
+            deltaTime = accelerationEvent.deltaTime;
+        }
+
+        // Variables
+
+        public Vector3 acceleration;
+        public float deltaTime;
+    }
+}
