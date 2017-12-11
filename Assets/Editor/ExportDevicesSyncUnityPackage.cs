fee2ac8c0207911c232ab0ef6775d2616e5eb27d
Erwan Normand <normand.erwan@protonmail.com>
2017-12-10 19:01:59 -0500

    Added an editor script to export the core package, and the examples package.

---
+++ Assets/Editor/ExportDevicesSyncUnityPackage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+public static class ExportDevicesSyncUnityPackage
+{
+    [MenuItem("DevicesSyncUnity/Export package")]
+    public static void ExportPackage()
+    {
+        string[] projectContent = new string[]
+        {
+            "Assets/DevicesSyncUnity/Prefabs",
+            "Assets/DevicesSyncUnity/Scenes",
+            "Assets/DevicesSyncUnity/Scripts",
+            "Assets/DevicesSyncUnity/Scenes",
+        };
+        AssetDatabase.ExportPackage(projectContent, "DevicesSyncUnity.unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+
+        projectContent = new string[]
+        {
+            "Assets/DevicesSyncUnity/Examples"
+        };
+        AssetDatabase.ExportPackage(projectContent, "DevicesSyncUnity-Examples.unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+    }
+}
